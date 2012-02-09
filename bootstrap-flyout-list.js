b4e62ae9e97ccea5ba5224b33218d124ed3c34bc
Justin D'Arcangelo <justindarc@gmail.com>
2012-02-09 01:25:52 -0500

    Added temporary fix for auto-closing the drawer when a notecard is selected.

--- bootstrap-flyout-list.js
+++ bootstrap-flyout-list.js
@@ -3,32 +3,40 @@ $(function() {
   // TODO: Determine if this can be delegated.
   $('ul.flyout-list > li > a').each(function(evt) {
     var $this = $(this);
-    var listItem = $this.parent();
+    var $listItem = $this.parent();
     var href = $this.attr('href');
-    var modal = $(href);
-    var primaryButton = modal.find('a.btn.primary');
+    var $modal = $(href);
+    var $primaryButton = $modal.find('a.btn.primary');
     
-    modal.bind('hidden', function(evt) {
-      listItem.removeClass('active');
+    $modal.bind('hidden', function(evt) {
+      $listItem.removeClass('active');
     });
     
-    primaryButton.bind('click', function(evt) {
-      modal.modal('hide');
+    $primaryButton.bind('click', function(evt) {
+      $modal.modal('hide');
     });
   });
   
   $('ul.flyout-list').delegate('a', 'click', function(evt) {
     var $this = $(this);
-    var listItem = $this.parent();
+    var $listItem = $this.parent();
+    var $list = $listItem.parent();
+    var $slidingDrawer = $list.parent('.drawer');
     var href = $this.attr('href');
-    var modal = $(href);
+    var $modal = $(href);
     
-    listItem.addClass('active');
+    $listItem.addClass('active');
     
-    modal.modal({
+    $modal.modal({
       backdrop: true,
       keyboard: true,
       show: true
     });
+    
+    // TODO: REMOVE THIS!! - TEMP FIX TO AUTO-CLOSE DRAWER ON MODAL LAUNCH.
+    if ($slidingDrawer.size() === 1) {
+      var closeDrawer = $slidingDrawer.data('closeDrawer');
+      if (closeDrawer) closeDrawer();
+    }
   });
 });
